da86bf1236d64497ccaa3629510cd0ebf5a06274
beatoz-tom <tom.lee@beatoz.io>
2026-05-26 17:13:54 +0900

    feat: add StablecoinV2Client and StablecoinBTIP26ReceiverClient for enhanced stablecoin functionality

--- src/contract-client/index.ts
+++ src/contract-client/index.ts
@@ -2,12 +2,14 @@
 export * from './linker-channel-client';
 export * from './linker-endpoint-client';
 export * from './stablecoin-client';
+export * from './stablecoin-v2-client';
+export * from './stablecoin-btip26-receiver-client';
 export * from './stablecoin-issuance-client';
 export * from './stablecoin-issuance-client-v2';
 export * from './token-btip10-client';
 export * from './btip10-stablecoin-client';
 export * from './permission-stable-btip10-client';
-export * from "./token-btip10-core";
+export * from './token-btip10-core';
 
 // Types
 export * from './type/issue-stablecoin';
